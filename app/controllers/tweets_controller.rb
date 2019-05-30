8a34e3da7f49b0278f4ab84364e833611f0966f0
daleksprinter <b1015120@fun.ac.jp>
2019-05-30 22:49:39 +0900

    get tweet in tweet model and render to index

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,10 +2,7 @@ require 'dotenv'
 Dotenv.load
 class TweetsController < ApplicationController
     def index
-        @tweets = Tweet.all
-        getTweet
-
-
+        @tweets = getTweet
     end
 
     private
@@ -16,15 +13,21 @@ class TweetsController < ApplicationController
         access_token = ENV['ACCESSTOKEN']
         access_token_secret = ENV['ACCESSTOKENSECRET']
 
-        @client = Twitter::REST::Client.new do |config|
+        client = Twitter::REST::Client.new do |config|
             config.consumer_key        = api_key
             config.consumer_secret     = api_secret_key
             config.access_token        = access_token
             config.access_token_secret = access_token_secret
         end
 
-        tweet_object = @client.status("1100643999193260032")
-        p tweet_object.full_text
+        tweets = Tweet.all
+        arr = []
+        tweets.each do |tweet|
+            tweet_object = client.status(tweet.tweet_id)
+            arr.push(tweet_object)
+        end
+        
+        return arr
     end
 
 end
